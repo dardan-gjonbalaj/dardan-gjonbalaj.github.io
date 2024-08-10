ab0c97af884ca5324475f9291182c87948d05051
Dardan Gjonbalaj <xdundun@gmail.com>
2024-08-10 15:44:45 +0200

    installed react removed source

--- src/index.js
+++ src/index.js
@@ -1,72 +1,17 @@
-console.clear();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const TAIL_LENGTH = 20;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const cursor = document.getElementById("cursor");
-
-let mouseX = 0;
-let mouseY = 0;
-
-let cursorCircles;
-let cursorHistory = Array(TAIL_LENGTH).fill({ x: 0, y: 0 });
-
-function onMouseMove(event) {
-  mouseX = event.clientX;
-  mouseY = event.clientY;
-}
-
-function initCursor() {
-  for (let i = 0; i < TAIL_LENGTH; i++) {
-    let div = document.createElement("div");
-    div.classList.add("cursor-circle");
-    cursor.append(div);
-  }
-  cursorCircles = Array.from(document.querySelectorAll(".cursor-circle"));
-}
-
-function updateCursor() {
-  cursorHistory.shift();
-  cursorHistory.push({ x: mouseX, y: mouseY });
-
-  for (let i = 0; i < TAIL_LENGTH; i++) {
-    let current = cursorHistory[i];
-    let next = cursorHistory[i + 1] || cursorHistory[TAIL_LENGTH - 1];
-
-    let xDiff = next.x - current.x;
-    let yDiff = next.y - current.y;
-
-    current.x += xDiff * 0.35;
-    current.y += yDiff * 0.35;
-    cursorCircles[i].style.transform = `translate(${current.x}px, ${
-      current.y
-    }px) scale(${i / TAIL_LENGTH})`;
-  }
-  requestAnimationFrame(updateCursor);
-}
-
-document.addEventListener("mousemove", onMouseMove, false);
-
-initCursor();
-updateCursor();
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
